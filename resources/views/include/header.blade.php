58981b2d37e8dc69907a7d070564e58548688b02
Sudhakar Reddy <sudhakar.edulakanti@gmail.com>
2023-12-26 01:07:28 +0530

    add registration logic validation2

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar w/ text</a>
+    <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,10 +10,13 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="{{route('login')}}">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="{{route('registration')}}">Registration</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('logout')}}">Logout</a>
         </li>
       </ul>
       <span class="navbar-text">
@@ -22,3 +25,4 @@
     </div>
   </div>
 </nav>
+
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post');
 Route::get('/registration', [AuthManager::class, 'registration'])->name('registration');
 Route::post('/registration', [AuthManager::class, 'registrationPost'])->name('registration.post');
 Route::get('/logout', [AuthManager::class, 'logout'])->name('logout');
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/profile',function(){
+        return "Hi";
+    });
+});
